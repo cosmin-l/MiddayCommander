fb55c69534581c2b469c2189ae840176c705e07c
Lechintan Cosmin <lechintan.cosmin@gmail.com>
2020-11-02 14:29:16 +0200

    refactor buttons creation

---
+++ buttons.js
@@ -0,0 +1,52 @@
+var blessed = require('blessed');
+
+const createButtons = () => {
+    let buttonStylePrefix = '{black-fg}{white-bg}{bold}';
+    let buttonStyleSuffix = '{/bold}{/white-bg}{/black-fg}';
+    let buttons = [];
+
+    let f1Button = addFooterButton(buttonStylePrefix + 'F1' + buttonStyleSuffix + ' Help      ', 1);
+    let f2Button = addFooterButton(buttonStylePrefix + 'F2' + buttonStyleSuffix + ' Menu        ', 10);
+    let f3Button = addFooterButton(buttonStylePrefix + 'F3' + buttonStyleSuffix + ' View        ', 20);
+    let f4Button = addFooterButton(buttonStylePrefix + 'F4' + buttonStyleSuffix + ' Edit       ', 30);
+    let f5Button = addFooterButton(buttonStylePrefix + 'F5' + buttonStyleSuffix + ' Copy        ', 40);
+    let f6Button = addFooterButton(buttonStylePrefix + 'F6' + buttonStyleSuffix + ' Move        ', 50);
+    let f7Button = addFooterButton(buttonStylePrefix + 'F7' + buttonStyleSuffix + ' Mkdir      ', 60);
+    let f8Button = addFooterButton(buttonStylePrefix + 'F8' + buttonStyleSuffix + ' Delete      ', 70);
+    let f9Button = addFooterButton(buttonStylePrefix + 'F9' + buttonStyleSuffix + ' PullDn      ', 80);
+    let f10Button = addFooterButton(buttonStylePrefix + 'F10' + buttonStyleSuffix + ' Quit       ', 90);
+
+    buttons.push(f1Button);
+    buttons.push(f2Button);
+    buttons.push(f3Button);
+    buttons.push(f4Button);
+    buttons.push(f5Button);
+    buttons.push(f6Button);
+    buttons.push(f7Button);
+    buttons.push(f8Button);
+    buttons.push(f9Button);
+    buttons.push(f10Button);
+
+    return buttons;
+}
+
+const addFooterButton = (text, pos) => {
+    let footerText = blessed.text({
+      content: text,
+      top: '100%-1',
+      height: '1',
+      left:`${pos}%`,
+      tags: true,
+      style: {
+        fg: 'white',
+        bg: 'lightblue',
+        border: {
+          fg: '#f0f0f0'
+        },
+      }
+    
+    })
+    return footerText;
+}
+
+exports.createButtons = createButtons;
--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-var blessed = require('blessed');
+const blessed = require('blessed');
+const buttonsAPI = require('./buttons');
+ 
  
 // Create a screen object.
 var screen = blessed.screen({
@@ -6,7 +8,7 @@ var screen = blessed.screen({
   autoPadding : true
 });
  
-screen.title = 'Iron Commander';
+screen.title = 'Midday Commander';
  
 let background = blessed.box({
   top: '2%',
@@ -59,40 +61,10 @@ let rightPanel = blessed.box({
   }
 });
 
-
-let buttonStylePrefix = '{black-fg}{white-bg}{bold}';
-let buttonStyleSuffix = '{/bold}{/white-bg}{/black-fg}';
-
-
-let f1Button = addFooterButton(buttonStylePrefix + 'F1' + buttonStyleSuffix + ' Help      ', 1);
-screen.append(f1Button);
-
-let f2Button = addFooterButton(buttonStylePrefix + 'F2' + buttonStyleSuffix + ' Menu        ', 10);
-screen.append(f2Button);
-
-let f3Button = addFooterButton(buttonStylePrefix + 'F3' + buttonStyleSuffix + ' View        ', 20);
-screen.append(f3Button);
-
-let f4Button = addFooterButton(buttonStylePrefix + 'F4' + buttonStyleSuffix + ' Edit       ', 30);
-screen.append(f4Button);
-
-let f5Button = addFooterButton(buttonStylePrefix + 'F5' + buttonStyleSuffix + ' Copy        ', 40);
-screen.append(f5Button);
-
-let f6Button = addFooterButton(buttonStylePrefix + 'F6' + buttonStyleSuffix + ' Move        ', 50);
-screen.append(f6Button);
-
-let f7Button = addFooterButton(buttonStylePrefix + 'F7' + buttonStyleSuffix + ' Mkdir      ', 60);
-screen.append(f7Button);
-
-let f8Button = addFooterButton(buttonStylePrefix + 'F8' + buttonStyleSuffix + ' Delete      ', 70);
-screen.append(f8Button);
-
-let f9Button = addFooterButton(buttonStylePrefix + 'F9' + buttonStyleSuffix + ' PullDn      ', 80);
-screen.append(f9Button);
-
-let f10Button = addFooterButton(buttonStylePrefix + 'F10' + buttonStyleSuffix + ' Quit       ', 90);
-screen.append(f10Button);
+let buttons = buttonsAPI.createButtons();
+buttons.forEach(button => { 
+  screen.append(button);
+});
 
 screen.append(background);
 screen.append(leftPanel);
@@ -109,23 +81,3 @@ leftPanel.focus();
  
 // Render the screen.
 screen.render();
-
-
-function addFooterButton(text, pos){
-  let footerText = blessed.text({
-    content: text,
-    top: '100%-1',
-    height: '1',
-    left:`${pos}%`,
-    tags: true,
-    style: {
-      fg: 'white',
-      bg: 'lightblue',
-      border: {
-        fg: '#f0f0f0'
-      },
-    }
-  
-  })
-  return footerText;
-}
